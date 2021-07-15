c6bca1538b7d65237c938d0bf88fa224f001fa49
Fırat Selim ZİREKOĞLU <zirekoglu18@itu.edu.tr>
2021-07-15 03:42:24 +0300

    fixed socket

--- App.js
+++ App.js
@@ -12,7 +12,6 @@ function NicknameScreen({navigation}) {
 
   useEffect(() => {
     if(!btnClick) return;
-    console.log('Buton tıklanıldı')
     fetch('https://simple-chat-server-20210711.herokuapp.com/login',{
       method: 'POST',
       headers: {
@@ -48,7 +47,7 @@ function NicknameScreen({navigation}) {
 function MessageBaloon({nick, message, time, gOra}){
   return(
     <View style={gOra?styles.messageBaloon0Con:styles.messageBaloon1Con}>
-       <View style={[styles.messageBaloon,gOra?styles.messageBaloon0:styles.messageBaloon1,{color:"#"}]}>
+       <View style={[styles.messageBaloon,gOra?styles.messageBaloon0:styles.messageBaloon1]}>
       <Text>{nick}</Text>
       <Text>{message}</Text>
       <View style={styles.timeCon}><Text style={styles.time}>{time}</Text></View>
@@ -59,34 +58,48 @@ function MessageBaloon({nick, message, time, gOra}){
 
 function ChatScreen({route, navigation}) {
   const { nick,token } = route.params;
-  const [data,setData] =useState({nick,token});
   const [messages,setMessages]=useState([
-    {nick:"system", message:'Hello '+data.nick,time:"00.00",gOra:true},
+    {nick:"system", message:'Hello '+nick, time:"00.00",gOra:true},
   ]);
   const [btnClick,setBtnClick]=useState(false);
   const [message,setMessage]=useState('');
   const [socket, setSocket] = useState(null);
-  const [socketConnected, setSocketConnected] = useState(false);
       useEffect(()=>{
-          setSocket(IO("https://simple-chat-server-20210711.herokuapp.com",{query:{token:token}}));
-          console.log('connect tekrar oldu')
-        },[])
-        useEffect(() => {
-          if (!socket) return;
-          socket.on('connect', (d) => {
+        const sckt=IO("https://simple-chat-server-20210711.herokuapp.com",{transports:["websocket"],query:{token:token}});
+         /* sckt.on('connect', (d) => {
             console.log("connected",d);
-            setSocketConnected(socket.connected);
+            subscribeToDateEvent();
+          });
+          sckt.on('disconnect', () => {
+          });*/
+          setSocket(sckt);
+        },[])
+        useEffect(()=>{
+          if(!socket) return;
+          socket.on('connect', () => {
+            console.log("connected");
+            subscribeToDateEvent();
           });
           socket.on('disconnect', () => {
-            setSocketConnected(socket.connected);
           });
-       
-        }, [socket]);
+          socket.on('chat',d=>{
+            d.gOra=d.token==token?false:true;
+            setMessages(state=>[...state,d]);
+          });
+        },[socket])
         useEffect(()=>{
-          if(btnClick){
-            socket.emit('chat','lalalalalallalaal');
+          if(!btnClick && message!=""){
+            setMessage("");
           }
-          },[btnClick])
+          if(!btnClick)return 
+            socket.emit('chat',{token, message, nick});
+            setBtnClick(false);
+
+          },[btnClick]);
+            // subscribe to socket date event
+  const subscribeToDateEvent = (interval = 1000) => {
+    socket.emit('subscribeToDateEvent', interval);
+  }
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <ScrollView style={{width:"100%", height:"100%", paddingVertical:15, marginBottom:40}}>
